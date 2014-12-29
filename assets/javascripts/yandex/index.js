8a28ea056a43440a265a37c0335578d3f0ee8e41
tab <tukhfatullin@gmail.com>
2014-12-29 22:55:26 +0600

    Automated commit at 2014-12-29 16:55:26 UTC by middleman-deploy 1.0.0

---
+++ assets/javascripts/yandex/index.js
@@ -0,0 +1,10 @@
+(function() {
+  $(function() {
+    return $('.js-download-files').click(function() {
+      if (typeof yaCounter21674110 !== "undefined" && yaCounter21674110 !== null) {
+        yaCounter21674110.reachGoal('js-download-files');
+      }
+    });
+  });
+
+}).call(this);
